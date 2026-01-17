204fc0f7004cc1e09aa644d7ede529409506b3aa
pawanxandromeda <pawanxandromeda@gmail.com>
2026-01-17 21:19:36 +0530

    c

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://alpex-backend-production.up.railway.app/api/',
+    baseURL: 'https://alpexbackend-production.up.railway.app/api/',
     headers: {
         'Content-Type': 'application/json',
     },
